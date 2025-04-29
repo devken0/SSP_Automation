024a5b6f83a07c3ea049f6a4e34dd109bd53ec58
Ken <kend3v@proton.me>
2025-04-29 16:37:32 +0800

    Add tests conditional looping

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-import pytest 
+import pytest
+import os
 
 if __name__ == "__main__":
-    # -s disables output capturing, allowing Pytest to read input properly.
-    pytest.main(["-s", "tests/", "--html=reports/test_report.html"])
+    choice = "y"
+    while choice == "y":
+        # -s disables output capturing, allowing Pytest to read input properly.
+        pytest.main(["-s", "tests/", "--html=reports/test_report.html"])
+        
+        os.system("wmctrl -a Terminal")
+        os.system("wmctrl -a Thonny")
+        choice = input("\nWould you like to run another test? (Y/n)").strip().lower()
+        if not choice:
+            choice = "y"
+        
+    
 
