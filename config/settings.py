07bb9de4e9fa58245a94b5185237366e397b74ea
Ken <kend3v@proton.me>
2025-05-06 13:20:15 +0800

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -3,3 +3,7 @@ CHROMIUM_PATH = "/usr/bin/chromium"
 WAIT_DURATION = 20
 BASE_URL = "https://pnbwtm106.pnbuat.local/customer.htm?type=working_cl&source=business"
 LOGIN_PAGE_URL = "https://pnbwtm106.pnbuat.local/portal/loginp.jsp"
+DEFAULT_PHONE = "09763853530"
+DEFAULT_BRANCH = "BGC-BURGOS CIRCLE"
+DEFAULT_PASSWORD = "testing123"
+DEFAULT_SPOUSE_EMAIL = "untitledtester37@gmail.com"
